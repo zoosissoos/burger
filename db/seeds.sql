fd756e2350b5728859d5427e9b7ddc0e18a3417f
dan <danbrianlewis@gmail.com>
2018-01-22 18:17:43 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Vegan Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jucy Burger", TRUE);
